9f287c17ad1eceaee531ad045f7f8b7847634bdd
Webgeek RUS <92749070+We6geek@users.noreply.github.com>
2023-12-04 05:31:03 +0300

    Add files via upload

---
+++ REVIVAL DRAGONS/scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".multiple-items").slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    centerMode: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
